eb792e633056316ae478aadc7b02d148009eb2cc
Sourav Pal <143326384+Souravpal08@users.noreply.github.com>
2024-08-26 11:08:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StrictMode } from 'react'
+
+import React,{ StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
